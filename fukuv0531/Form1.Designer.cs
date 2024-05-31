07f28788b63d8601ede3eb855631c5858ffb236c
YuTanaka <tanakaedu@users.noreply.github.com>
2024-05-31 13:58:56 +0900

    復習問題

--- fukuv0531/Form1.Designer.cs
+++ fukuv0531/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(621, 287);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(371, 213);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 45);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(621, 316);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- fukuv0531/Form1.cs
+++ fukuv0531/Form1.cs
@@ -2,9 +2,32 @@ namespace fukuv0531
 {
     public partial class Form1 : Form
     {
+        int vy = 15;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //label1.Top = label1.Top + vy;
+            label1.Top += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vy = -15;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vy = 15;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "‚½‚È‚©";
         }
     }
 }
